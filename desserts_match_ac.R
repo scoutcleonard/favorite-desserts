c4b776632f5b61a7115c141271fcc6b57300f528
AllieCole <icole@bren.ucsb.edu>
2021-08-23 15:43:18 -0700

    chocolate alone is not iconic

--- desserts_match_ac.R
+++ desserts_match_ac.R
@@ -13,3 +13,19 @@ dessert_listing <- dessert_elements %>%
   head(.,-3) %>%  # 3 last ones were not desserts 
   rowid_to_column("rank") %>%  # adding a column using the row number as proxy for the rank
   write_csv("data/iconic_desserts.csv") # save it as csv
+
+
+# is my desert on the list?
+
+allie_fav <- dessert_listing %>% 
+  filter(dessert_listing$dessert == "Chocolate")
+
+
+
+
+
+
+
+
+
+
